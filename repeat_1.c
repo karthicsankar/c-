d621ec64ba54152952f89512baddfda59dacc1d1
karthicsankar <35293999+karthicsankar@users.noreply.github.com>
2018-02-09 16:24:34 +0530

    repeat_1.c

---
+++ repeat_1.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a=0,i;
+char b[20];
+cout<<"enter the word";
+cin>>b;
+cout<<"\nenter the repeat timees:";
+cin>>a;
+for(i=0;i<=a;i++)
+
+{
+	cout<<"\n"<<b;
+
+}
+
+	return 0;
+}
